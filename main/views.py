81728442c1ba8d2bc3c7620be31574876d32a78c
zakriaalmorissi <134652828+zakriaalmorissi@users.noreply.github.com>
2024-02-24 21:05:35 +0300

    upload my new version

--- main/views.py
+++ main/views.py
@@ -1,21 +1,27 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
+import datetime
 from .models import *
-# Create your views here.
 
 def home(request):
     # upload my profolio information
     profile = Myprofile.objects.get(id=2)
-    print(profile.name.first_name)
     introduction = Introduction.objects.get(id=1)
     # upload all posts to the home page     
-    post = Post.objects.all()
+    posts = Post.objects.all()
+    # upload the posts that are related to the specified category 
+    category_posts = Post.objects.filter(category__name="New projects")
+    # upload the posts that are related to the videos category
+    video_category = Post.objects.filter(category__name="videos")
 
     context = {
         'profile': profile,
         'introduction':introduction,
-        'posts': post
+        'posts': posts,
+        'projects': category_posts,
+        'videos':video_category
     }
+
     return render(request,"home/home.html",context)
 
 def post_view(request,id):
